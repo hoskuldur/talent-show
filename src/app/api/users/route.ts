42abf2f4c3e08a57dc0663cb9fa17ee09c9870a8
hoskuldur <hoskuldur@reon.is>
2024-03-19 09:15:00 +0000

    POST PUT PATCH DELETE on /users

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,6 +1,17 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    return NextResponse.json({ message: "Hello, World!" });
+    return NextResponse.json({ message: "John GET" });
 }
     
+export async function POST() {
+    return NextResponse.json({ message: "John POST" });
+}
+
+export async function PUT() {
+    return NextResponse.json({ message: "John PUT" });
+}
+
+export async function DELETE() {
+    return NextResponse.json({ message: "John DELETE" });
+}
