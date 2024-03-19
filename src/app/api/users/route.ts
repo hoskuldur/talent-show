1d9c7d3f50bdb68e3c0c3d670457835dd3398e64
hoskuldur <hoskuldur@reon.is>
2024-03-19 14:36:29 +0000

    Functioning GET request for users

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,7 +1,23 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
-    return NextResponse.json({ message: "User GET" });
+// export async function GET(req: NextRequest) {
+//     const params = req.headers.get("params");
+//     return NextResponse.json({ users });
+// }
+
+export async function GET(req: NextRequest & { query: { id?: string } }) {
+    const { id } = req.query;
+    // Check if id is present in the query
+    return id
+        ? // If id is present, search for the user
+          (() => {
+              const user = users.find(user => user.id === Number(id));
+              return user
+                  ? NextResponse.json({ user }) // User found
+                  : NextResponse.json({ message: "User not found" }, { status: 404 }); // User not found
+          })()
+        : // If no id is present, return all users
+          NextResponse.json({ users });
 }
     
 export async function POST() {
